64ecd55a13de9d2b2824119564fb598261df1e54
Dev Profile <kishore.rajkumar@hotmail.com>
2024-04-20 18:02:28 +0530

    new project created, springboot with configmap

---
+++ springboot-k8s-configmap/src/test/java/com/kishore/springbootk8sconfigmap/SpringbootK8sConfigmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishore.springbootk8sconfigmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootK8sConfigmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
