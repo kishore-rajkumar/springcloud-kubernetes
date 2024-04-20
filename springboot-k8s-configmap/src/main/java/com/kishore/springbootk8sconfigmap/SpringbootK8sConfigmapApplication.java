3d4a3da262e4212f2ab81112577e9ce85f798320
Dev Profile <kishore.rajkumar@hotmail.com>
2024-04-20 17:57:34 +0530

    new project created, springboot with configmap

---
+++ springboot-k8s-configmap/src/main/java/com/kishore/springbootk8sconfigmap/SpringbootK8sConfigmapApplication.java
@@ -0,0 +1,13 @@
+package com.kishore.springbootk8sconfigmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootK8sConfigmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootK8sConfigmapApplication.class, args);
+	}
+
+}
